61af70c51ac736294d46467d100ea00b4d6e83d2
Thaweewat <104571571+RThaweewat@users.noreply.github.com>
2023-03-24 19:41:12 +0700

    Update app.py

--- app.py
+++ app.py
@@ -71,9 +71,46 @@ if uploaded_file is not None:
         min_highschool_filtered = filtered_data.query("is_highschool == 1")
 
         st.subheader("Results")
-        st.write("Optimal thresholds:", optimal_thresholds)
+        st.write("Optimal thresholds (Without constraints) :", optimal_thresholds)
         st.write("Specialty ratios:", specialty_ratios)
         st.write("Number of people who pass:", len(filtered_data))
         st.write("Number of highschool who pass:", len(min_highschool_filtered))
         st.write(f"Pass ratio: {np.floor(len(filtered_data) / len(data) * 100)} %")
         st.write(f"Cut at: class >= {filtered_data['class_score'].min()} and quiz >= {filtered_data['quiz_score'].min()}")
+        
+else:
+    data = pd.read_csv("data.csv")
+    st.sidebar.header("Constraints")
+    min_total_pass = st.sidebar.number_input("Minimum total pass", min_value=0, max_value=None, value=100)
+    max_total_pass = st.sidebar.number_input("Maximum total pass", min_value=0, max_value=None, value=110)
+    min_highschool = st.sidebar.number_input("Minimum highschool students", min_value=0, max_value=None, value=15)
+    min_class_score = st.sidebar.number_input("Minimum class score", min_value=0, max_value=15, value=5)
+    min_quiz_score = st.sidebar.number_input("Minimum quiz score", min_value=0, max_value=11, value=5)
+    use_specialty_threshold = st.sidebar.checkbox("Use specialty threshold", value=False)
+
+    min_specialty_ratio = {
+        "Technology": st.sidebar.slider("Minimum technology ratio", min_value=0.0, max_value=1.0, value=0.3, step=0.01),
+        "Business": st.sidebar.slider("Minimum business ratio", min_value=0.0, max_value=1.0, value=0.3, step=0.01),
+        "Design": st.sidebar.slider("Minimum design ratio", min_value=0.0, max_value=1.0, value=0.3, step=0.01),
+    }
+
+    if st.button("Calculate optimal thresholds"):
+        optimal_thresholds = find_optimal_thresholds(
+            data, min_total_pass, max_total_pass, min_highschool, min_specialty_ratio, use_specialty_threshold, min_class_score, min_quiz_score
+        )
+
+        filtered_data = filter_passing_candidates(data, *optimal_thresholds, min_class_score, min_quiz_score)
+
+        specialty_counts = filtered_data['specialty'].value_counts()
+        total_pass = len(filtered_data)
+        specialty_ratios = (specialty_counts / total_pass).to_dict()
+        min_highschool_filtered = filtered_data.query("is_highschool == 1")
+
+        st.subheader("Results")
+        st.write("Optimal thresholds (Without constraints) :", optimal_thresholds)
+        st.write("Specialty ratios:", specialty_ratios)
+        st.write("Number of people who pass:", len(filtered_data))
+        st.write("Number of highschool who pass:", len(min_highschool_filtered))
+        st.write(f"Pass ratio: {np.floor(len(filtered_data) / len(data) * 100)} %")
+        st.write(f"Cut at: class >= {filtered_data['class_score'].min()} and quiz >= {filtered_data['quiz_score'].min()}")
+    
